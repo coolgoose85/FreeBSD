427ab7281c6af5143935f6c34740539c8c8dea48
coolgoose85 <vaibhav123@gmail.com>
2014-08-02 13:18:35 -0700

    3

---
+++ sys/netinet6/ip6.h
@@ -0,0 +1,4 @@
+/*	$FreeBSD: src/sys/netinet6/ip6.h,v 1.4 2000/03/11 20:44:53 shin Exp $	*/
+/*	$KAME: ip6.h,v 1.7 2000/03/25 07:23:36 sumikawa Exp $	*/
+
+#error "netinet6/ip6.h is obsolete.  use netinet/ip6.h"
